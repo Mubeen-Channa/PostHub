34d0bd27099b5a44302f16163a7d6b65f5f0f200
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2025-09-04 11:15:13 +0500

    Added test model and route to fetch data from MongoDB Atlas - Phase 1.5

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors');
 const dotenv = require('dotenv')
+
+// Routes
+const TestRoute = require('./routes/Test');
+const LoginRoute = require('./routes/Login');
+
+const app = express()
 
 dotenv.config();
 
-const app = express()
+app.use(express.json());
+app.use(cors());
 
 const uri = process.env.MONGO_URI;
 
@@ -13,12 +21,12 @@ mongoose.connect(uri)
 .catch( err => console.log("Error: " + err));
 
 app.get('/', (req, res) => {
-    res.redirect('/Login')
+    res.redirect('/login')
 })
 
-app.get('/login', (req, res) => {
-    res.send("Welcome to Login Page")
-})
+app.use('/test', TestRoute)
+app.use('/login', LoginRoute)
+
 
 app.listen('5000', (req, res) => {
     console.log("PostHub Server is running on port 5000")
