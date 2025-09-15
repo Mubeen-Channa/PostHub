4c3c33a389fa81d131ecd08e9bd6462cab1d3b42
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2025-09-15 10:24:22 +0500

    Fixed BASE_URL for Netlify build - Phase 1.5

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,2 @@
-// export const BASE_URL = "https://3.110.164.160:5000";
-export const BASE_URL = "https://cors-anywhere.herokuapp.com/http://3.110.164.160:5000";
-
+// export const BASE_URL = "http://3.110.164.160:5000";
+export const BASE_URL = "https://3.110.164.160.nip.io"
